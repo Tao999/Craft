3807c7f4ec343f8a77982daf625f5c68fe311af6
Dalfrak <blitzhacker31@gmail.com>
2019-02-27 11:56:44 +0100

    onsenfoutpourcettefois

---
+++ src/view/AppMainView.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppMainView extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO
+	}
+
+}
